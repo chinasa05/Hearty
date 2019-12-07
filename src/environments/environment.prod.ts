2740e1b1c0ec726bd8c9f3a3275bd91bf526f9a0
Chinasa Njoku <amandanjoku05@gmail.com>
2019-12-07 15:14:52 -0500

    initial shell app with firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyA62wqQKM7wyiWcfj_fB_uaVVCAxMYc6oc",
+  authDomain: "hearti-8779f.firebaseapp.com",
+  databaseURL: "https://hearti-8779f.firebaseio.com",
+  projectId: "hearti-8779f",
+  storageBucket: "hearti-8779f.appspot.com",
+  messagingSenderId: "8025218519",
+  appId: "1:8025218519:web:0dc0550aa6acdea78ebf92",
+  measurementId: "G-LTPZ6TQ8E5"}
+};
