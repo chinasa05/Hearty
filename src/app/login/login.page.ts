ab73320105b35a111e803223642f2ff11eeb1339
Chinasa Njoku <amandanjoku05@gmail.com>
2019-12-08 16:26:36 -0500

    yes end of camp

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../core/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { User } from 'firebase';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,13 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+ 
+  constructor(private authService: AuthService, private afAuth: AngularFireAuth, private router: Router) { }
 
-  ngOnInit() { }
-
+  ngOnInit() { this.afAuth.authState.subscribe({
+    next: (credentials: User) => {
+      if (credentials) {
+        this.router.navigate(['']);
+      }
+    },
+    error: (err: Error) => {
+      console.warn('Reach to i-fit');
+    }
+  });}
   
+
+
     googleLogin(): void {
     console.log( 'google login clicked');
+    this.authService.googleLogin();
     } 
 
   
